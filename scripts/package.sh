02cac7b61f3eb8b106d0ab02249083a5c25983c8
TheBrokenRail <connor24nolan@live.com>
2020-12-12 15:58:32 -0500

    Remove Minifier, Causes Too Many Issues

--- scripts/package.sh
+++ scripts/package.sh
@@ -8,18 +8,10 @@ mkdir debian/tmp
 rm -rf out
 mkdir -p out
 
-# Add PIP To PATH
-export PATH="${HOME}/.local/bin:${PATH}"
-
 # Copy
 mkdir -p debian/tmp/opt/mcpil-r
 rsync -r debian/common/ debian/tmp
 rsync -r src/ debian/tmp/opt/mcpil-r
-minify() {
-    # $1 = Input
-    find "$1" -name '*.py' -exec sh -c 'OUT="$(pyminify --rename-globals --remove-literal-statements "$1")"; echo "${OUT}" > "$1"' -- {} \;
-}
-minify debian/tmp/opt/mcpil-r
 
 # Substitute
 sed -i 's/${VERSION}/'"$(cat src/VERSION)"'/g' debian/tmp/DEBIAN/control
@@ -29,4 +21,4 @@ mkdir out/deb
 dpkg -b debian/tmp out/deb
 
 # Clean Up
-#rm -rf debian/tmp
+rm -rf debian/tmp
