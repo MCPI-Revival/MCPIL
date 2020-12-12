e281ded29926647ba9bf965a2595963d31a1f555
TheBrokenRail <connor24nolan@live.com>
2020-12-12 14:22:56 -0500

    Add Package Action

--- src/mcpil-r.py
+++ src/mcpil-r.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 #  mcpil-r.py
@@ -20,8 +19,7 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
 
 import signal
 
